04de0725a15d7a576a0a72f6798e4902f285bb3a
dcheley <danielcheley@hotmail.com>
2018-04-21 15:25:49 -0400

    Adding placeholder image, creating home page & signup page plus general css styles

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -3,4 +3,10 @@ class BusinessesController < ApplicationController
 
   def index
   end
+
+  private
+
+  def business_params
+    params.require(:business).permit(:name, :address, :category, :phone)
+  end
 end
